b48aaa42276c75f1b0d31b9fa21217bbfce118ae
shander3377 <65767417+shander3377@users.noreply.github.com>
2022-11-20 17:51:21 +0530

    fully done

--- client.py
+++ client.py
@@ -28,9 +28,11 @@ class GUI:
 
     def goAhead(self, name):
         self.login.destroy()
-        self.name= name
+        # self.name= name
         rcv = Thread(target=self.recieve)
         rcv.start()
+        self.layout(name)
+
 
     def recieve(self):
         global client
@@ -40,11 +42,66 @@ class GUI:
                 if msg.lower() == "nickname":
                     client.send(self.name.encode("utf-8"))
                 else:
-                    pass
+                    self.show_msg(msg)
             except:
-                print("An error occured")
+                print("An error occured!")
                 client.close()
-                break;
+                break
+
+    def on_closing(self):
+        print("yes")
+        global client
+        client.close()
+        self.window.destroy()
+    def send(self, msg):
+        self.textArea.config(state=DISABLED)
+        self.msg = msg
+        self.msgEntry.delete(0, END)
+        snd = Thread(target=self.write)
+        snd.start()
+    def write(self):
+        global client
+        self.textArea.config(state=DISABLED)
+        while(True):
+            message = (f"{self.name}: {self.msg}")
+            client.send(message.encode("utf-8"))
+            self.show_msg(message)
+            break
+    def show_msg(self, message):
+        print(message)
+        self.textArea.config(state=NORMAL)
+        self.textArea.insert(END, message+"\n\n")
+        self.textArea.config(state=DISABLED)
+        self.textArea.see(END)
+    def layout(self, name):
+        self.name = name
+        self.window.deiconify()
+        self.window.title("Chat Room")
+        self.window.resizable(width=False, height=False)
+        self.window.configure(width=470, height=550, bg="#17202A")
+
+        self.nameLabel = Label(self.window, text=name, font="sans-serif 12", bg="#17202A", fg="cyan")
+        self.nameLabel.place(relx=0.5, rely=0.05, anchor=CENTER)
+
+        self.lineLabel = Label(self.window, width=450,  bg="cyan")
+        self.lineLabel.place(relx=0, rely = 0.1, relheight=0.012)
+
+        self.textArea = Text(self.window, width=20, height=2, bg="#17202A", fg="cyan", font="sans-serif 12", padx=5, pady=5)
+        self.textArea.config(state=DISABLED)
+        self.textArea.place(relwidth=1, rely=0.12, relheight=0.745)
+        self.labelBottom = Label(self.window, bg="cyan", fg="black", font="sans-serif 14 bold", height=18)
+        self.labelBottom.place(relwidth=1, rely=0.825)
+        self.msgEntry = Entry(self.labelBottom, width=80, bg="#17202A", fg="cyan")
+        self.msgEntry.place(relwidth=0.80, relheight=0.2, rely=0.008, relx=0.011)
+
+        self.sendButton = Button(self.labelBottom, text="Send", font="sans-serif 14", command=lambda: self.send(self.msgEntry.get()))
+        self.sendButton.place(relx=0.84, rely=0.035)
+
+        self.scrollBar = Scrollbar(self.textArea)
+        self.scrollBar.place(relheight=1, relx=0.974)
+        self.scrollBar.config(command=self.textArea.yview)
+
+        self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
 
 GUI()       
 
